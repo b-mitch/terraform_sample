7d86556a060faddd7305bd66510b4fa289a6e986
b-mitch <bmitchum.dev@gmail.com>
2024-01-20 08:34:05 -0500

    updates security group config

--- security-group.tf
+++ security-group.tf
@@ -49,6 +49,22 @@ resource "aws_security_group" "dev_webserver_security_group" {
     vpc_id      = aws_vpc.vpc.id
 
     ingress {
+        description = "http access"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "https access"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
         description = "SSH access"
         from_port   = 22
         to_port     = 22
