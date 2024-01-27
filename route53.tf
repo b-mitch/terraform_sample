010431d3e9d60982cda535efa691b3dde692af8a
b-mitch <bmitchum.dev@gmail.com>
2024-01-27 08:03:11 -0500

    creates route 53 config file

---
+++ route53.tf
@@ -0,0 +1,17 @@
+# Create an A record for the root domain
+resource "aws_route53_record" "woutfh_domain" {
+  zone_id = "Z07136312IOFHVADAEBK6"  # Created through the console before purchasing the domain
+  name    = "woutfh.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["44.193.200.207"]  
+}
+
+# Create an A record for the www subdomain
+resource "aws_route53_record" "www_subdomain" {
+  zone_id = "Z07136312IOFHVADAEBK6"  # Created through the console before purchasing the domain
+  name    = "www.woutfh.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["44.193.200.207"] 
+}
