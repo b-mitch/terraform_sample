9aea253f581dff4b64aeca688e61e95f7f9c422a
b-mitch <bmitchum.dev@gmail.com>
2024-01-31 15:11:19 -0500

    assigns target group weights to variables

--- alb.tf
+++ alb.tf
@@ -74,12 +74,12 @@ resource "aws_lb_listener" "http_listener" {
       forward {
         target_group {
           arn    = aws_lb_target_group.blue.arn
-          weight = 100
+          weight = var.blue_weight
         }
 
         target_group {
           arn    = aws_lb_target_group.green.arn
-          weight = 0
+          weight = var.green_weight
         }
 
         stickiness {
