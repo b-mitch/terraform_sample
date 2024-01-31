930a378df647d1b2cc1863257c80a2e8985c1f66
b-mitch <bmitchum.dev@gmail.com>
2024-01-31 13:53:56 -0500

    updates alb config for blue-green deployment

--- alb.tf
+++ alb.tf
@@ -13,10 +13,10 @@ resource "aws_lb" "alb" {
     }
 }
 
-# create target group for the application load balancer
+# create target group for the blue instance
 # terraform aws create target group
-resource "aws_lb_target_group" "target_group" {
-    name        = "target-group"
+resource "aws_lb_target_group" "blue" {
+    name        = "blue-group"
     target_type = "instance"
     port        = 80
     protocol    = "HTTP"
@@ -26,7 +26,7 @@ resource "aws_lb_target_group" "target_group" {
         healthy_threshold   = 2
         interval            = 30
         matcher             = "200,301,302"
-        path = "/health"
+        path = "/"
         port = "traffic-port"
         protocol            = "HTTP"
         timeout             = 5
@@ -34,7 +34,31 @@ resource "aws_lb_target_group" "target_group" {
     }
 
     tags = {
-        Name = "target-group"
+        Name = "blue-group"
+    }
+}
+# create target group for the green instance
+# terraform aws create target group
+resource "aws_lb_target_group" "green" {
+    name        = "green-group"
+    target_type = "instance"
+    port        = 80
+    protocol    = "HTTP"
+    vpc_id      = aws_vpc.vpc.id
+
+    health_check {
+        healthy_threshold   = 2
+        interval            = 30
+        matcher             = "200,301,302"
+        path = "/"
+        port = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = 5
+        unhealthy_threshold = 2
+    }
+
+    tags = {
+        Name = "green-group"
     }
 }
 
@@ -46,23 +70,59 @@ resource "aws_lb_listener" "http_listener" {
     protocol          = "HTTP"
 
     default_action {
-        target_group_arn = aws_lb_target_group.target_group.arn
+        target_group_arn = aws_lb_target_group.blue.arn
         type             = "forward"
     }
 }
 
 # create target group attachment for the production ec2 instance in az1
 # terraform aws create target group attachment
-resource "aws_lb_target_group_attachment" "target_group_attachment_az1" {
-    target_group_arn = aws_lb_target_group.target_group.arn
+resource "aws_lb_target_group_attachment" "blue_group_attachment_az1" {
+    target_group_arn = aws_lb_target_group.blue.arn
     target_id        = aws_instance.prod_instance_az1.id
     port             = 80
 }
 
 # create target group attachment for the production ec2 instance in az2
 # terraform aws create target group attachment
-resource "aws_lb_target_group_attachment" "target_group_attachment_az2" {
-    target_group_arn = aws_lb_target_group.target_group.arn
+resource "aws_lb_target_group_attachment" "green_group_attachment_az2" {
+    target_group_arn = aws_lb_target_group.green.arn
     target_id        = aws_instance.prod_instance_az2.id
     port             = 80
+}
+
+# define listener rules for blue group with heavier weight
+# terraform aws create listener rules
+resource "aws_lb_listener_rule" "rule1" {
+  listener_arn = aws_lb_listener.http_listener.arn
+  priority     = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.blue.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
+
+# define listener rules for green group with lighter weight
+# terraform aws create listener rules
+resource "aws_lb_listener_rule" "rule2" {
+  listener_arn = aws_lb_listener.http_listener.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.green.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
 }
